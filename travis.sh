368fd80f7e02fb667147984d006e4207102f0416
Masami HIRATA <msmhrt@gmail.com>
2014-01-05 08:45:52 +0900

    Fixed

--- travis.sh
+++ travis.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
-echo pip install -r test-requirements.txt
-pip install -r test-requirements.txt
 echo pip freeze -r test-requirements.txt
 pip freeze -r test-requirements.txt
 echo pip freeze -l -r test-requirements.txt
 pip freeze -l -r test-requirements.txt
 echo pip freeze -r -r test-requirements.txt
-pip freeze -r -r test-requirements.txt
+pip freeze --dep -r test-requirements.txt
 echo set
 set
-echo ls -al $home
-ls -al $home
+echo ls -al $HOME
+ls -al $HOME
+echo ls -al /home/travis/virtualenv/python2.6/bin
+ls -al /home/travis/virtualenv/python2.6/bin
+echo ls -al /usr/local/pypy/bin
+ls -al /usr/local/pypy/bin
+echo ls -al /usr/local/gradle/bin
+ls -al /usr/local/gradle/bin
+echo ls -al /usr/local/sbin
+ls -al /usr/local/sbin
+echo ls -al /usr/local/bin
+ls -al /usr/local/bin
+echo ls -al /usr/sbin
+ls -al /usr/sbin
+echo ls -al /usr/bin
+ls -al /usr/bin
+echo ls -al /sbin
+ls -al /sbin
+echo ls -al /bin
+ls -al /bin
